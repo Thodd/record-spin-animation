4a464e320b36dce39ad24dda0bf8fe26b77c42a4
Thorsten Hochreuter <thodd@thodd.net>
2025-06-18 19:23:11 +0200

    Settings Dialog - final

--- src/renderer.js
+++ src/renderer.js
@@ -10,6 +10,26 @@ const Renderer = {
     recordlabel(imageDataUrl) {
         const recordLabel = document.getElementById("record_label");
         recordLabel.style.backgroundImage = `url(${imageDataUrl})`;
+    },
+
+    updateAll(values) {
+        // background color
+        const designer = document.getElementById("designer");
+        designer.style.backgroundColor = `${values.background ?? "#ffffff"}`;
+
+        const bgImage = document.getElementById("background_image");
+        // show bg image
+        bgImage.style.display = values.showBackground ? "initial" : "none";
+        // blur;
+        bgImage.style.filter = `blur(${values.blur ?? 10}px)`;
+
+        // plastic wrapper
+        const plasticWrap = document.getElementById("plastic_wrap");
+        plasticWrap.style.opacity = `${values.wrapperOpacity ?? 0.5}`;
+
+        // values.scale;
+        const designerScaler = document.getElementById("designer-scaler");
+        designerScaler.style.transform = `scale(${values.scale ?? 1})`;
     }
 }
 
--- src/settings-dialog.js
+++ src/settings-dialog.js
@@ -1,3 +1,5 @@
+import Renderer from "./renderer.js";
+
 const SettingsDialog = {
     init() {
         // window handle
@@ -34,6 +36,20 @@ const SettingsDialog = {
         const openButton = document.getElementById("open-dialog");
         openButton.addEventListener("click", () => {
             dialog.style.display = "initial";
+        });
+
+        // connect change handlers to input elements
+        document.querySelectorAll("#settings-dialog input").forEach((inputElement) => {
+            inputElement.addEventListener("change", (e) => {
+                // we simply update everything even if just one value changed, who cares 🤷
+                Renderer.updateAll({
+                    background: document.getElementById("settings-background-color").value,
+                    showBackground: document.getElementById("settings-show-background").checked,
+                    blur: document.getElementById("settings-blur").value,
+                    wrapperOpacity: document.getElementById("settings-wrapper-opacity").value,
+                    scale: document.getElementById("settings-scale").value
+                })
+            });
         });
     }
 }
